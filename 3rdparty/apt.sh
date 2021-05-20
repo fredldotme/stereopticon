858da902896a9b0d0073498f1c08c8f9c474fe0b
Alfred Neumayer <dev.beidl@gmail.com>
2021-05-20 21:25:42 +0200

    3rdparty: Install apt deps for mir

--- 3rdparty/apt.sh
+++ 3rdparty/apt.sh
@@ -17,4 +17,31 @@ sudo apt install -y \
     libfontconfig1-dev \
     libssl-dev \
     libusb-1.0-0-dev \
-    libusb-dev
+    libusb-dev \
+    libwayland-dev \
+    libnvidia-egl-wayland-dev \
+    libegl-dev \
+    eglexternalplatform-dev \
+    libgles2-mesa-dev \
+    libglm-dev \
+    libprotobuf-dev \
+    capnproto \
+    libcapnp-dev \
+    libgoogle-glog-dev \
+    liblttng-ust-dev \
+    libudev-dev \
+    libxcb-composite0-dev \
+    libxcursor-dev \
+    libdrm-dev \
+    libgbm-dev \
+    libepoxy-dev \
+    nettle-dev \
+    libxkbcommon-dev \
+    libinput-dev \
+    libxml++2.6-dev \
+    libyaml-cpp-dev \
+    python3-pil \
+    libumockdev-dev \
+    wlcs \
+    protobuf-compiler \
+    systemtap-sdt-dev
