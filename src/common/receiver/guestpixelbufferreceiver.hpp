69ea7db602948a9663464a781c45a1c304cd3e51
Alfred Neumayer <dev.beidl@gmail.com>
2021-05-26 18:51:09 +0200

    src: Begin reading pixel buffers in separate thread

--- src/common/receiver/guestpixelbufferreceiver.hpp
+++ src/common/receiver/guestpixelbufferreceiver.hpp
@@ -1,10 +1,46 @@
 #ifndef GUESTPIXELBUFFERRECEIVER_H
 #define GUESTPIXELBUFFERRECEIVER_H
 
+#include <unistd.h>
+#include <thread>
 #include "../guestpixelbuffer.h"
 
+struct ReadSocketThreadMemory {
+	bool read = true;
+	bool done = false;
+	int socketFd = -1;
+};
+
+void socketReadLoop(ReadSocketThreadMemory* memory)
+{
+	while (memory->read) {
+		
+	}
+	memory->done = true;
+}
+
 class GuestPixelBufferReceiver {
-	
+public:
+	GuestPixelBufferReceiver(int socketFd)
+	{
+		this->m_readLoopMemory.socketFd = socketFd;
+		this->m_readThread = new std::thread(socketReadLoop, &m_readLoopMemory);
+	}
+	~GuestPixelBufferReceiver()
+	{
+		this->m_readLoopMemory.read = false;
+		if (this->m_readThread) {
+			while(!this->m_readLoopMemory.done) {
+				usleep(1000);
+			}
+			delete this->m_readThread;
+			this->m_readThread = nullptr;
+		}
+	}
+
+private:
+	std::thread* m_readThread = nullptr;
+	ReadSocketThreadMemory m_readLoopMemory;
 };
 
 #endif
