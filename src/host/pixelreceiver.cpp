fa44cf94b7ebb2e0225ab13403efe4cdfb3260e2
Alfred Neumayer <dev.beidl@gmail.com>
2021-05-27 22:31:37 +0200

    host: Implement spawning host windows on request

--- src/host/pixelreceiver.cpp
+++ src/host/pixelreceiver.cpp
@@ -1,9 +1,29 @@
 #include "pixelreceiver.h"
 
 
-void PixelReceiver::spawnWindow(GuestWindowSpawnData& spawnData)
+void PixelReceiver::spawnWindow(GuestWindowSpawnCommand& spawnData)
 {
-    // TODO
+    if (this->windowExists(spawnData.header.windowId))
+        return;
+
+    auto hostWindow = std::make_shared<HostWindowPair>();
+    hostWindow->window = SDL_CreateWindow("",
+                                         spawnData.data.x,
+                                         spawnData.data.y,
+                                         spawnData.data.width,
+                                         spawnData.data.height,
+                                         SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL);
+    if (!hostWindow->window)
+        return;
+
+    hostWindow->renderer = SDL_CreateRenderer(hostWindow->window,
+                                              -1,
+                                              0);
+
+    if (!hostWindow->renderer)
+        return;
+
+    this->m_waylandWindowIdMap.insert({spawnData.header.windowId, hostWindow});
 }
 
 void PixelReceiver::receiveRedraw(GuestPixelBufferRedrawCommand& command)
--- src/host/pixelreceiver.h
+++ src/host/pixelreceiver.h
@@ -2,17 +2,37 @@
 #define PIXELRECEIVER_H
 
 #include <map>
+#include <memory>
+#include <SDL.h>
 #include "../common/receiver/guestpixelbufferreceiver.hpp"
+
+struct HostWindowPair
+{
+    HostWindowPair() {}
+    ~HostWindowPair()
+    {
+        if (renderer) {
+            SDL_DestroyRenderer(renderer);
+            renderer = nullptr;
+        }
+        if (window) {
+            SDL_DestroyWindow(window);
+            window = nullptr;
+        }
+    }
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+};
 
 class PixelReceiver : public PixelBufferHandler
 {
 public:
-    void spawnWindow(GuestWindowSpawnData& spawnData) override;
+    void spawnWindow(GuestWindowSpawnCommand& spawnData) override;
     void receiveRedraw(GuestPixelBufferRedrawCommand& command) override;
     bool windowExists(wayland_window_id_t& windowId) override;
 
 private:
-    std::map<wayland_window_id_t, int> m_waylandWindowIdMap;
+    std::map<wayland_window_id_t, std::shared_ptr<HostWindowPair> > m_waylandWindowIdMap;
 };
 
 #endif
