604ef3104e38710d344f4296e5c45b03407595b4
Alfred Neumayer <dev.beidl@gmail.com>
2021-05-28 19:57:41 +0200

    build: Fix loading default number of CPUs on macOS

--- build.sh
+++ build.sh
@@ -19,13 +19,20 @@ BUILD_DEPS=0
 BUILD_GUEST=0
 BUILD_HOST=0
 
-# Overridable number of build processors
-if [ "$NUM_PROCS" == "" ]; then
-    NUM_PROCS=$(nproc --all)
-fi
-
 # Platform-specific checking
 KERNEL_NAME=$(uname -s)
+
+# Overridable number of build processors
+if [ "$NUM_PROCS" == "" ]; then
+    if [ "$KERNEL_NAME" == "Linux" ]; then
+        NUM_PROCS=$(nproc --all)
+    elif [ "$KERNEL_NAME" == "Darwin" ]; then
+        NUM_PROCS=$(sysctl -n hw.ncpu)
+    else
+        # Default to 4 build processors on unknown systems
+        NUM_PROCS=4
+    fi
+fi
 
 # Argument parsing
 while [[ $# -gt 0 ]]; do
@@ -76,8 +83,8 @@ function build_cmake {
     ADDITIONAL_C_FLAGS=""
     ADDITIONAL_CXX_FLAGS=""
     if [ "$KERNEL_NAME" == "Linux" ]; then
-        ADDITIONAL_C_FLAGS="-Wl,-rpath-link,$INSTALL/lib""
-        ADDITIONAL_CXX_FLAGS="-Wl,-rpath-link,$INSTALL/lib""
+        ADDITIONAL_C_FLAGS="-Wl,-rpath-link,$INSTALL/lib"
+        ADDITIONAL_CXX_FLAGS="-Wl,-rpath-link,$INSTALL/lib"
     fi
 
     PKG_CONF_SYSTEM=/usr/lib/$MULTIARCH/pkgconfig
